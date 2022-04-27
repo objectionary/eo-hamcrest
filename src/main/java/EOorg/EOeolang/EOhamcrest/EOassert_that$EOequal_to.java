2c5ca209048e324269f3c10d34590b3bced0bc3a
graur <ancongr@yandex.ru>
2022-04-27 14:48:52 +0300

    (#2) - introduce assert-that object

---
+++ src/main/java/EOorg/EOeolang/EOhamcrest/EOassert_that$EOequal_to.java
@@ -0,0 +1,53 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Graur Andrew
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+// @checkstyle PackageNameCheck (1 line)
+package EOorg.EOeolang.EOhamcrest;
+
+import org.eolang.*;
+
+/**
+ * Assert-that.equal-to.
+ *
+ * @since 0.1
+ * @checkstyle TypeNameCheck (100 lines)
+ */
+@SuppressWarnings("PMD.AvoidDollarSigns")
+public class EOassert_that$EOequal_to extends PhDefault {
+
+    /**
+     * Ctor.
+     * @param sigma The \sigma
+     * @checkstyle BracketsStructureCheck (200 lines)
+     */
+    @SuppressWarnings("PMD.ConstructorOnlyInitializesOrCallOtherConstructors")
+    public EOassert_that$EOequal_to(final Phi sigma) {
+        super(sigma);
+        this.add("x", new AtFree());
+        this.add("φ", new AtComposite(this, rho -> {
+            final Object expected = new Dataized(rho.attr("x").get()).take(Object.class);
+            final Object actual = new Dataized(rho.attr("ρ").get()).take(Object.class);
+            return new Data.ToPhi(expected.equals(actual));
+        }));
+    }
+}
